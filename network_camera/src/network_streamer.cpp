9b493668a0555b33b0de72313573029adf904863
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-10-12 18:00:20 -0400

    Move #E

---
+++ network_camera/src/network_streamer.cpp
@@ -0,0 +1,97 @@
+/*
+ * network_streamer.cpp
+ *
+ *  Created on: Oct 12, 2015
+ *      Author: murat
+ */
+
+/*
+ * camera_node.cpp
+ *
+ *  Created on: Sep 27, 2015
+ *      Author: murat
+ */
+
+/*
+ * publisher.cpp
+ *
+ *  Created on: Sep 16, 2015
+ *      Author: murat
+ */
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "cv_bridge/cv_bridge.h"
+#include "sensor_msgs/image_encodings.h"
+#include "sensor_msgs/Image.h"
+#include "image_transport/image_transport.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <time.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/videoio/videoio.hpp"
+
+struct cameraObject{
+	std::string ip;
+	std::string port;
+	std::string streamURL;
+};
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "network_streamer");
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
+	image_transport::Publisher imagePublisher = it.advertise("/networkCam", 1);
+
+    cameraObject networkCamera;
+    networkCamera.ip = "http://192.168.1.76";
+    networkCamera.port = "81";
+    networkCamera.streamURL = "/videostream.cgi?loginuse=admin&loginpas=12345&dummy=param.mjpg";
+
+    cv::namedWindow("Network Stream",1);
+    std::string fullURL = networkCamera.ip + ":" + networkCamera.port + networkCamera.streamURL;
+    std::cout<<"URL to stream from: "<<fullURL<<std::endl;
+    cv::VideoCapture capture(fullURL);
+    std::cout<<"capture object initialized"<<std::endl;
+
+    cv::Mat frame;
+    std::cout<<"frame created"<<std::endl;
+
+    cv_bridge::CvImage rosImage;
+
+	rosImage.encoding = "bgr8";
+	rosImage.header.stamp = ros::Time::now();
+	rosImage.header.frame_id = "bar";
+	std::cout<<"ros image initialized"<<std::endl;
+
+    while(ros::ok()){
+//    	frame = cv::imread("~/Desktop/opencv-logo-small.png");
+//    	capture>>frame;
+    	cv::Mat frame2;
+    	capture.read(frame2);
+    	std::cout<<"frame acquired"<<std::endl;
+    	if(frame.empty()){
+    		std::cout<<"Frame is empty"<<std::endl;
+    		continue;
+    	}
+    	else{
+    		rosImage.image = frame.clone();
+			imagePublisher.publish(rosImage.toImageMsg());
+			cv::imshow("Network Stream",frame);
+			if(cv::waitKey(30) >= 0)
+				break;
+    	}
+    	ros::spinOnce();
+	}
+
+	return 0;
+}
+
+
+
